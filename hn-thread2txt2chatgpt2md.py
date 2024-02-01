6b4dd28dd4dbc8e69ea3589dc6b942b66e4df565
Knut Behrends <knb@gfz-potsdam.de>
2024-02-01 10:57:37 +0100

    Refactor: split send_to_llm() function into smaller fcts

--- hn-thread2txt2chatgpt2md.py
+++ hn-thread2txt2chatgpt2md.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-import sys
 import os
+import sys
 import re
 import subprocess
 from urllib.parse import urlparse
 import requests
 import json
 from dotenv import load_dotenv
+import argparse
 
 # import time # for sleep(), not used yet
 
@@ -22,120 +23,154 @@ def get_item_id(hnitem):
 def download_hn_thread(hnitem, outfile):
     subprocess.run(['lynx', '--dump', hnitem], stdout=open(outfile, 'w'))
 
-def preprocess(outfile, intermediate_file, final_file):
+def preprocess(outfile, intermediate_file, final_file, topic):
 
     # remove HEADER 
     with open(outfile, 'r') as f:
         lines = f.readlines()
-
+    # add topic string to end of first line
+    lines[0] = lines[0].strip() + f" {topic}\n"
     line_start = next(i for i, line in enumerate(lines) if "login" in line)
     lines = lines[line_start:]
 
-    with open(intermediate_file, 'w') as f:
-        for line in lines:
-            if not any(s in line for s in [']next', ']reply', '[s.gif']):
-                f.write(line)
+# if intermediate file does not exist, preprocess it
+    if os.path.isfile(intermediate_file):
+        # send to standard error
+        print(f"File {outfile} already exists, skipping preprocess.", file=sys.stderr)
+    
+    else:
+        with open(intermediate_file, 'w') as f:
+            for line in lines:
+                if not any(s in line for s in [']next', ']reply', '[s.gif']):
+                    f.write(line)
+        # POSTS: remove lines with "ago" 
+        with open(intermediate_file, 'r') as f:
+            lines = f.readlines()
 
-
-    # POSTS: remove lines with "ago" 
-    with open(intermediate_file, 'r') as f:
-        lines = f.readlines()
-
-    with open(intermediate_file, 'w') as f:
-        for line in lines:
-            f.write(re.sub(r'^(\s*\d+\.\w+) .+ ago .+$', r'\1', line))
+        with open(intermediate_file, 'w') as f:
+            for line in lines:
+                f.write(re.sub(r'^(\s*\d+\.\w+) .+ ago .+$', r'\1', line))
 
 
     # FOOTER: remove lines starting with  "Guidelines...FAQ...Lists...API...Security" 
-    with open(intermediate_file, 'r') as f:
-        lines = f.readlines()
+        with open(intermediate_file, 'r') as f:
+            lines = f.readlines()
 
-    with open(intermediate_file, 'w') as f:
-        for line in lines:
-            f.write(re.sub(r'^\s*\[\d+\].–.$', '', line))
+        with open(intermediate_file, 'w') as f:
+            for line in lines:
+                f.write(re.sub(r'^\s*\[\d+\].–.$', '', line))
 
-    line_end = next(i for i, line in enumerate(lines) if re.search(r'Guidelines.+FAQ.+Lists|.+API.+Security', line))
+        line_end = next(i for i, line in enumerate(lines) if re.search(r'\]Guidelines', line))
 
 
     # write to final file
-    with open(intermediate_file, 'r') as f:
-        lines = f.readlines()
+        with open(intermediate_file, 'r') as f:
+            lines = f.readlines()
 
-    with open(final_file, 'w') as f:
-        for line in lines[:line_end]:
-            f.write(line)
+        with open(final_file, 'w') as f:
+            for line in lines[:line_end]:
+                f.write(line)
 
-def chunk_text(text, chunk_size=14000):
+def chunk_text(text, chunk_size=10000):
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
-## LLM : ChatGPT-3.5 API
-def send_to_llm(url, instruction, topic, chunks, first_response):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {API_KEY}"
-    }
-    instruction = "\n".join([topic,instruction])
+def chunks_data(chunks):
+    chunks_data = []
     for chunk in chunks:
-        data = {
-            "model": "gpt-3.5-turbo-16k",
-            "messages": [
-                {
-                    "role": "assistant",
-                    "content": instruction
-                },
-                {
-                    "role": "user",
-                    "content": chunk
-                }
-            ],
-            "temperature": 0.1,
-            "top_p": 1,
-            "n": 1,
-            "stream": False,
-            "max_tokens": 2500,
-            "presence_penalty": 0,
-            "frequency_penalty": 0
-        }
-        data = json.dumps(data)
-        response = requests.post(url, headers=headers, data=data)
+        chunks_data.append(set_data(config['model'], chunk, instruction))
+    print(f"Number of data chunks: {len(chunks_data)}", file=sys.stderr)
+    return chunks_data
+    
+def set_data(model, chunk, instruction):
+    
+    if config['model'] != 'gpt-3.5-turbo-16k':
+        freq_penalty = 0.1
+    else:
+        freq_penalty = 0.0
+    data = {"model": model,
+        "messages": [
+            {
+                "role": "assistant",
+                "content": instruction
+            },
+            {
+                "role": "user",
+                "content": chunk
+            }
+        ],
+        "temperature": 0.1,
+        "top_p": 1,
+        "n": 1,
+        "stream": False,
+        "max_tokens": 2500,
+        "presence_penalty": 0,
+        "frequency_penalty": freq_penalty
+    }
+    return json.dumps(data)
+
+def create_headers(api_key):
+    return {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+## LLM : ChatGPT-3.5 API
+def send_to_llm(config, headers, topic, chunks, first_response_flag):
+    i = 1
+    for chunk in chunks:
+        print(f"chunk {i} of {len(chunks)} posted to {config['url']}, model {config['model']}]", file=sys.stderr)
+        response = requests.post(config['url'], headers=headers, data=chunk)
         response_json = response.json()
         
         if response.status_code == 200:
-            if first_response:
+            if first_response_flag:
                 print(f"topic: {topic} | ... | ...", topic)
-                first_response = False
+                first_response_flag = False
             print(response_json['choices'][0]['message']['content'])
         else:
-            print(f"Error: {response_json['error']['message']}")
+            print(f"Error: {response_json['error']['message']}", file=sys.stderr)
+        i += 1
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python hn-thread2txt2chatgpt2md.py <hacker-news-item-url> <topic> [outfile]")
-        print("e.g. python hn-thread2txt2chatgpt2md.py https://news.ycombinator.com/item?id=37156101 'Why did people in the past look so much older' hacker-news-thread.txt")
-        sys.exit(0)
+    parser = argparse.ArgumentParser(description='Process Hacker News threads.')
+    parser.add_argument('hnitem', help='Hacker News item URL')
+    parser.add_argument('topic', help='Topic of the discussion', default="Hacker news thread")
+    parser.add_argument('--outfile', help='Output file', default=None)
+    parser.add_argument('--api_key', help='OpenAI API key', required=True)
+    parser.add_argument('--model', help='Model to use for the LLM', default='gpt-3.5-turbo-16k')
+    parser.add_argument('--url', help='URL for the OpenAI API', default='https://api.openai.com/v1/chat/completions')
+    args = parser.parse_args()
 
-    hnitem = sys.argv[1]
-    topic = sys.argv[2]
-    outfile = sys.argv[3] if len(sys.argv) > 3 else f"hacker-news-thread-{get_item_id(hnitem)}.txt"
+    outfile = args.outfile if args.outfile else f"hacker-news-thread-{get_item_id(args.hnitem)}.txt"
     outfile = os.path.join("output", outfile)
     intermediate_file = f"{outfile}-intermediate.txt"
-    final_file = f"{outfile}-for-chatgpt.txt"
+    final_file = f"{outfile}-input-for-llm.txt"
 
-    download_hn_thread(hnitem, outfile)
-    preprocess(outfile, intermediate_file, final_file)
+    # if file does not exist, download it
+    if not os.path.isfile(outfile):
+        download_hn_thread(args.hnitem, outfile)
+    else:
+        print(f"File {outfile} already exists, skipping download.", file=sys.stderr)
+    preprocess(outfile, intermediate_file, final_file, args.topic)
     
     with open(final_file, 'r') as f:
         text = f.read()
-    # Reading the instruction from a file in the "input" subdirectory
+    
+    print(f"Read {final_file}...{len(text)}  chars read.", file=sys.stderr)
     instruction_file_path = "input/instruction.txt"
     with open(instruction_file_path, 'r') as f:
         instruction = f.read()
 
-    # send topic, instruction, and text to ChatGPT-3.5 API
-    # Output will be a markdown table sent to stdout
-    topic_line = ": ".join(["The topic was", topic])
-    first_response = True
-    chunks = chunk_text(text)
-    API_KEY = os.getenv('OPENAI_API_KEY')
-    url = "https://api.openai.com/v1/chat/completions"
-    send_to_llm(instruction, topic_line, chunks, first_response)
+    config = {
+        'api_key': args.api_key,
+        'model': args.model,
+        'url': args.url
+    }
+    
+    topic_line = ": ".join(["The topic was", args.topic])
+    instruction = "\n".join([topic_line,instruction])
+    first_response_flag = True
+    headers = create_headers(config['api_key'])
+    chunks_rawtext = chunk_text(text)
+    chunks_data = chunks_data(chunks_rawtext)
+    send_to_llm(config, headers, topic_line, chunks_data, first_response_flag)
