1c60bf0a3bf2f8eb2687fd48a4e23f21a2e4af6f
Knut Behrends <knb@gfz-potsdam.de>
2024-05-21 20:43:16 +0200

    Set default LLM to gemini-pro (largest context window)

--- script_attic/hacker-news-summarizer.sh
+++ script_attic/hacker-news-summarizer.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 # Provide a one-page summary of a Hacker News discussion
-# from: Simon Willison
-# https://github.com/simonw/llm/, together with the llm-perplexity plugin and the llm-gemini plugin
+# by: Simon Willison
+# https://github.com/simonw/llm/, 
+# together with the llm-perplexity plugin and the llm-gemini plugin (by others)
 
-# Validate that the argument is an integer
+# Validate that the first argument is an integer
 if [[ ! $1 =~ ^[0-9]+$ ]]; then
-  echo "Please provide a valid integer as the argument."
+  echo "Please provide a valid integer as the first argument."
   exit 1
 fi
 
-# Make API call, parse and summarize the discussion
-# Set default value for $2 if it's not provided
+id="$1"
+
+# Parse the optional -m argument. 
+# If not provided, use the default model to gemini-pro (has largest context window)
 if [[ -z $2 ]]; then
-  set -- "$1" "llama-3-70b-instruct"
+  set -- "$1" "gemini-pro"
 fi
-
-# Validate that the $2 argument is a string without blanks
-if [[ $2 =~ \  ]]; then
-  echo "Please provide a string without blanks as the second argument, or omit it entirely."
-  exit 1
-fi
-
-MODEL=$2
-echo using $MODEL
-
-curl -s "https://hn.algolia.com/api/v1/items/$1" | \
-  jq -r 'recurse(.children[]) | .text' | \
-  llm -m $MODEL 'Summarize the themes of the opinions expressed here, including quotes where appropriate.'
-
-#  llm -m gemini-pro-latest 'Summarize the themes of the opinions expressed here, including quotes where appropriate.'
+model=$2
+echo using $model
+# Make API call, parse and summarize the discussion
+curl -s "https://hn.algolia.com/api/v1/items/$id" | \
+  jq -r 'recurse(.children[]) | .author + ": " + .text' | \
+  llm -m "$model" 'Summarize the themes of the opinions expressed here.
+  For each theme, output a markdown header.
+  Include direct "quotations" (with author attribution) where appropriate.
+  You MUST quote directly from users when crediting them, with double quotes.
+  Fix HTML entities. Output markdown. Go long.'
+#  llm -m gemini-pro 'Summarize the themes of the opinions expressed here, including quotes where appropriate.'
 #  llm -m mixtral-8x7b-instruct 'Summarize the themes of the opinions expressed here, including quotes where appropriate.'
 #  llm -m llama-3-70b-instruct 'Summarize the themes of the opinions expressed here, including quotes where appropriate.'
