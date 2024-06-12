4e668925b53fd03d61d4861c1d502575a07788e6
Knut Behrends <knb@gfz-potsdam.de>
2024-06-12 17:31:04 +0200

    Add instruction: Write 1 short paragraph about each topic

--- script_attic/hacker-news-summarizer.sh
+++ script_attic/hacker-news-summarizer.sh
@@ -23,6 +23,7 @@ echo using $model
 curl -s "https://hn.algolia.com/api/v1/items/$id" | \
   jq -r 'recurse(.children[]) | .author + ": " + .text' | \
   llm -m "$model" 'Summarize the themes of the opinions expressed here.
+  Write a short paragraph with details about each theme of the discussion.
   For each theme, output a markdown header.
   Include direct "quotations" (with author attribution) where appropriate.
   You MUST quote directly from users when crediting them, with double quotes.
